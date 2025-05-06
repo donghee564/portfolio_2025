864a2e691093381903d732b8bcbe9b2857cbeb81
Donghee Han <poohaha8974@gmail.com>
2025-05-07 00:40:11 +0900

    메인 문구 추가

--- src/components/SectionHome/SectionHome.jsx
+++ src/components/SectionHome/SectionHome.jsx
@@ -18,16 +18,14 @@ const SectionHome = () => {
   const text2 = "DONGHEE's";
   const text3 = "PORTFOLIO";
   const text4 = "디자인을 움직임으로 연결하는 퍼블리셔, 한동희입니다.";
-  // const text5 =
-  //   "특히 금융권 프로젝트를 경험하면서 정보의 명확한 전달과 사용자 편의성이 중요한 요소라는";
-  // const text6 = "점을 깊이 이해하게 되었습니다.";
+  const text5 =
+    "본 포트폴리오는 React.js와 Framer Motion을 사용하여 제작되었습니다.";
 
   const delay1 = 0;
   const delay2 = calculateDelay(text1, delay1);
   const delay3 = calculateDelay(text2, delay2, true);
   const delay4 = calculateDelay(text3, delay3, true);
-  // const delay5 = calculateDelay(text4, delay4);
-  // const delay6 = calculateDelay(text5, delay5);
+  const delay5 = calculateDelay(text4, delay4);
 
   return (
     <section id="home" className="section-container" aria-label="홈 섹션">
@@ -57,12 +55,9 @@ const SectionHome = () => {
             <span className="text-wrapper">
               <AnimatedText text={text4} blockDelay={delay4} />
             </span>
-            {/* <span className="text-wrapper">
+            <span className="text-wrapper">
               <AnimatedText text={text5} blockDelay={delay5} />
             </span>
-            <span className="text-wrapper">
-              <AnimatedText text={text6} blockDelay={delay6} />
-            </span> */}
           </p>
         </div>
       </div>
